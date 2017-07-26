2b98c026aa6be6df0bb22ac25f7ecdf38a94e6e1
jjk376 <jea376@naver.com>
2017-07-26 13:44:26 +0900

    건드린 파일들.

--- src/main/java/com/naver/zipviewer/controller/FileController.java
+++ src/main/java/com/naver/zipviewer/controller/FileController.java
@@ -1,44 +1,33 @@
 package com.naver.zipviewer.controller;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.naver.zipviewer.domain.FileVO;
 import com.naver.zipviewer.service.FileService;
 
-@Controller
+@RestController
 public class FileController {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	private static long fileId = 0;
 	@Autowired private FileService service;
 	
 	@GetMapping(value = "/files")
 	public ResponseEntity<?> list(Model model)
 	{
-		List<FileVO> voList = null;
 		try
 		{
-			voList = service.listAll();
-			return new ResponseEntity<>(voList, HttpStatus.OK);
+			return new ResponseEntity<>(service.listAll(), HttpStatus.OK);
 		}
 		catch(Exception e)
 		{
@@ -52,32 +41,9 @@ public class FileController {
 	@PostMapping(value = "/files")
 	public ResponseEntity<?> insert(@RequestPart("file") MultipartFile file, Model model)
 	{
-		FileVO vo = new FileVO();
 		try
 		{
-			vo.setFileId(fileId++);
-			vo.setUserId("admin");	
-	
-			InputStream is = file.getInputStream();
-			byte[] buffer = new byte[1024 * 8];
-			FileOutputStream fos = new FileOutputStream(new File("C:\\Users\\upload", file.getOriginalFilename()));
-			while(true)
-			{
-				int count = is.read(buffer);
-				if(count == -1)
-					break;
-				fos.write(buffer, 0, count);
-			}
-		
-			vo.setFileName(file.getOriginalFilename());		
-			vo.setFileSize(file.getSize());
-			vo.setFileUploadTime(System.currentTimeMillis());
-	  
-			is.close();
-			fos.close();
-		
-			service.insert(vo);
-			return new ResponseEntity<>(vo, HttpStatus.CREATED);
+			return new ResponseEntity<>(service.insert(file), HttpStatus.CREATED);
 		}
 		catch(Exception e)
 		{
--- src/main/java/com/naver/zipviewer/service/FileService.java
+++ src/main/java/com/naver/zipviewer/service/FileService.java
@@ -1,8 +1,16 @@
 package com.naver.zipviewer.service;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.naver.zipviewer.domain.FileVO;
 import com.naver.zipviewer.persistence.FileDAO;
 
@@ -11,11 +19,33 @@ public class FileService {
 	
 @Autowired private FileDAO dao;
 	
-	public void insert(FileVO vo)
+	public FileVO insert(@RequestPart("file") MultipartFile file) throws Exception
 	{
-		dao.insert(vo);
-	}
+		FileVO vo = new FileVO();
+		vo.setUserId("admin");	
 	
+		InputStream is = file.getInputStream();
+		byte[] buffer = new byte[1024 * 8];
+		FileOutputStream fos = new FileOutputStream(new File("C:\\Users\\upload", file.getOriginalFilename()));
+		while(true)
+		{
+			int count = is.read(buffer);
+			if(count == -1)
+				break;
+			fos.write(buffer, 0, count);
+		}
+		
+		vo.setFileName(file.getOriginalFilename());		
+		vo.setFileSize(file.getSize());
+		vo.setFileUploadTime(System.currentTimeMillis());
+	  
+		is.close();
+		fos.close();
+		
+		dao.insert(vo);
+		return vo;
+	}
+
 	public List<FileVO> listAll()
 	{
 		return dao.listAll();
