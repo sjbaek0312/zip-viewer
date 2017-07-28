cd923fc7f6fe1c44c6f6fed9a7e2841e27f14098
jjk376 <jea376@naver.com>
2017-07-28 22:01:02 +0900

    개발 환경 설정.

--- src/fe/app/controller/FileListController.js
+++ src/fe/app/controller/FileListController.js
@@ -1,19 +1,3 @@
 /**
  * 
  */
-import DragAndDropAction from './DropHandler.js';
-class FileListController {
-	constructor(View, Model){
-		this._view = View;
-		this._model = Model;
-		this.bindDropEvents();
-		
-		this._model.apiFileList();
-	}
-	bindDropEvents(){
-		$("#dropZone").on("drop", {toModel : this._model}, DragAndDropAction.drop);
-		$("#dropZone").on("dragover",DragAndDropAction.dragover);
-	}
-}
-
-export default FileListController;
