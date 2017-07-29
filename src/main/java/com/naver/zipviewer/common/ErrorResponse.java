67506e1f15360f24632d85940e6892337d58f4ca
sjbaek0312 <sjbaek0312@naver.com>
2017-07-29 12:08:40 +0900

    Update ErrorResponse.java

--- src/main/java/com/naver/zipviewer/common/ErrorResponse.java
+++ src/main/java/com/naver/zipviewer/common/ErrorResponse.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.naver.zipviewer.common;
 
 public class ErrorResponse {
@@ -20,26 +19,3 @@ public class ErrorResponse {
 	}
 	
 }
-=======
-package com.naver.zipviewer.common;
-
-public class ErrorResponse {
-
-	private int errorCode;
-	private String msg;
-	
-	public int getErrorCode() {
-		return errorCode;
-	}
-	public void setErrorCode(int errorCode) {
-		this.errorCode = errorCode;
-	}
-	public String getMsg() {
-		return msg;
-	}
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
-	
-}
->>>>>>> 88e5bc6ffdb2841b4f5a1bd9734d26eda01b0431
