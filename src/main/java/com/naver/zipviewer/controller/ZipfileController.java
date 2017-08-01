3c7f05ef1cdfbbab725ffa41935a1c15a1472bd3
sjbaek0312 <sjbaek0312@naver.com>
2017-08-01 18:43:47 +0900

    sjbaek0312 / Zipfiles load

---
+++ src/main/java/com/naver/zipviewer/controller/ZipfileController.java
@@ -0,0 +1,33 @@
+package com.naver.zipviewer.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.naver.zipviewer.service.ZipfileService;
+
+@RestController
+@RequestMapping(value = "/api/files/{fileId}/zipfiles")
+public class ZipfileController {
+	
+	@Autowired private ZipfileService service;
+	@Value("#{config['fileUploadPath']}") String path;
+	
+	@PostMapping(value = "")
+	public ResponseEntity<?> load(@PathVariable(value = "fileId") long fileId, Model model)
+	{
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("items", service.load(fileId, path));
+		return new ResponseEntity<>(map, HttpStatus.CREATED);
+	}
+}
