4bcb15d7e25fd26c26657e1b68080f4827f0f083
Jeong jae kyeong <jea376@naver.com>
2017-08-02 11:21:09 +0900

    jQuery prefix Exchanges to $
    jQuery prefix를 $로 고침.

--- src/fe/app/view/FileListView.js
+++ src/fe/app/view/FileListView.js
@@ -1,23 +1,23 @@
 class FileListView {
 	constructor(domId){
 		console.log(domId+" view Create..");
-		this._dom = jQuery(domId);
+		this._dom = $(domId);
 	}
 
 	getDomForEventBinding(){
 		return this._dom;
 	}
 	rendering(json) {
-		let innerDiv = jQuery("<div></div>").addClass("col-xs-2 file").data("fileId",json.fileId);
-		let img = jQuery("<img class='media-object' style='height: 100px'></img>");
+		let innerDiv = $("<div></div>").addClass("col-xs-2 file").data("fileId",json.fileId);
+		let img = $("<img class='media-object' style='height: 100px'></img>");
 		img.attr("src", "/static/img/file-"+json.fileType+".png").attr("onerror","this.src='/static/img/file-common.png'");
 		
-		let name = jQuery("<h5></h5>").text(json.fileName).addClass("filename");
+		let name = $("<h5></h5>").text(json.fileName).addClass("filename");
 		innerDiv.append(img).append(name);
 	
 		let div;
 		if (this._dom.children().length%6 === 0)
-			div = jQuery("<div></div>").attr("class", "row");
+			div = $("<div></div>").attr("class", "row");
 		else
 			div = this._dom.children().last();
 		
